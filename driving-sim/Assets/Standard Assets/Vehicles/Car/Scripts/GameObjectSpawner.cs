3291e1d71f863ab1c7cf767235135fac3d7b3803
Anthony Morse <anthony.c.morse@gmail.com>
2017-09-02 20:12:10 -0500

    added speed bump prefab

--- driving-sim/Assets/Standard Assets/Vehicles/Car/Scripts/GameObjectSpawner.cs
+++ driving-sim/Assets/Standard Assets/Vehicles/Car/Scripts/GameObjectSpawner.cs
@@ -7,14 +7,23 @@ namespace UnityStandardAssets.Vehicles.Car
     public class GameObjectSpawner : MonoBehaviour
     {
         private WaypointProgressTracker progressTracker;
+        private GameObject spawnParent;
 
         public GameObject gameObject;
 
         public float gameObjectLifeSpan;
 
+        public float rotationX;
+        public float rotationY;
+        public float rotationZ;
+
         private void Awake()
         {
             this.progressTracker = this.GetComponent<WaypointProgressTracker>();
+
+            // Create a dummy game object in order to keep our spawned game objects
+            // contained neatly in the object heirarchy
+            this.spawnParent = new GameObject("Spawn Parent");
         }
 
         private void Update()
@@ -23,8 +32,15 @@ namespace UnityStandardAssets.Vehicles.Car
 
             var target = this.progressTracker.target;
 
-            var generatedSpeedBump = Instantiate(this.gameObject, target.position, target.rotation);
-            Destroy(generatedSpeedBump.gameObject, this.gameObjectLifeSpan);
+            var generatedGameObject = Instantiate(this.gameObject, target.position, target.rotation, this.spawnParent.transform);
+
+            // Some game objects need additional rotation for them to be oriented correctly,
+            // usually these rotations would be a multiple of 90.0 degrees
+            generatedGameObject.transform.Rotate(Vector3.right, this.rotationX, Space.Self);
+            generatedGameObject.transform.Rotate(Vector3.up, this.rotationY, Space.Self);
+            generatedGameObject.transform.Rotate(Vector3.forward, this.rotationZ, Space.Self);
+
+            Destroy(generatedGameObject.gameObject, this.gameObjectLifeSpan);
         }
     }
 }
